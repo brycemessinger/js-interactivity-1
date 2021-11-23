9bd946c9ecd3ab520d2b13aabd84a26a6ee474f7
bryce messinger <brycemessinger48@gmail.com>
2021-11-23 14:54:11 -0700

    all completed

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const addMovie = (event) => {
 
 const deleteMovie = (event) => {
     event.target.parentNode.remove();
-    message.textContent(`Movie Deleted!`)
+    message.textContent = event.target.previousSibling.textContent + `Movie Deleted!`
+    revealMessage();
 }
 
 const formSubmit = document.querySelector(`form`)
@@ -32,9 +33,18 @@ formSubmit.addEventListener('submit', addMovie);
 const crossOffMovie = (event) => {
     event.target.classList.toggle(`checked`)
     if(event.target.classList.contains('checked')){
-        message.textContent = `Movie Watched!`
-    }
+        message.textContent = event.target.textContent + `Movie Watched!`
+    }   
     else{
-        message.textContent = `Movie added back!`
+        message.textContent = event.target.textContent + `Movie added back!`
     }
+    revealMessage();
+}
+
+
+const revealMessage = () => {
+    message.classList.remove('hide');
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000);
 }
